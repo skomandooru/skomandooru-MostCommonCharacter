cae72984ed059a886625449fac666742476eacde
Surya Komandooru <skomandooru@gmail.com>
2023-09-23 14:40:28 +0000

    Passed

--- src/main/java/MostCommonCharacter.java
+++ src/main/java/MostCommonCharacter.java
@@ -1,3 +1,12 @@
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class MostCommonCharacter {
     /**
@@ -8,6 +17,33 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        Set<Character> charSet = new HashSet<>();
+        char[] strArr = str.toCharArray();
+        for (int i = 0; i < strArr.length; i++) {
+            charSet.add(strArr[i]);
+        }
+
+        Map<Character, Integer> charMap = new HashMap<>();
+            
+        for (char c : strArr) {
+            if (charMap.containsKey(c)) {
+                charMap.put(c, charMap.get(c) + 1);
+            } else {
+                charMap.put(c, 1);
+            }
+        }
+
+        LinkedHashMap<Character, Integer> sortedMap = charMap.entrySet()
+        .stream()
+        .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+        .collect(Collectors.toMap(
+            Map.Entry::getKey,
+            Map.Entry::getValue,
+            (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+    
+    System.out.println(sortedMap);
+      
+        return sortedMap.keySet().iterator().next();
     }
+    
 }
